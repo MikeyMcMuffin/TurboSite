81aa5018dafeaa2d331f06fae2e20d4218034559
Michael McCluskey <mcclum@verskolan.com>
2017-05-05 15:38:15 +0100

    test changes made for css menu and such

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,10 @@
 Mijans is a butt and so am I.
+<head>
+<link rel="stylesheet" type="text/css" href="/css/TurboSite.css">
+</head>
+<div id="mySidenav" class="sidenav">
+  <a href="#" id="about">About</a>
+  <a href="#" id="blog">Blog</a>
+  <a href="#" id="projects">Projects</a>
+  <a href="#" id="contact">Contact</a>
+</div>
