ba689ee5574c956f827cfd28ae1f4abd1ea36ba1
Michael McCluskey <mcclum@VERSKOLAN.COM>
2017-05-11 13:48:35 +0100

    #

--- public_html/index.php
+++ public_html/index.php
@@ -6,8 +6,7 @@
 	<script language="javascript" type="text/javascript"></script>
 </head>
 
-This is the dev code. 
-
+This is the dev code. Testing for git pull.
 <div class="sideMenu">
 	<a href="#" class="about"></a>
 	<a href="#" class="gallery"></a>
