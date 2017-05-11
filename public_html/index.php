75563c5bff096508cd92d751045344cabea8cda3
Michael McCluskey <mcclum@VERSKOLAN.COM>
2017-05-11 13:20:10 +0100

    Quick change to differ dev from live

--- public_html/index.php
+++ public_html/index.php
@@ -6,6 +6,8 @@
 	<script language="javascript" type="text/javascript"></script>
 </head>
 
+This is the dev code. 
+
 <div class="sideMenu">
 	<a href="#" class="about"></a>
 	<a href="#" class="gallery"></a>
